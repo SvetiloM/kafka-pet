3c87ac9ffce97ef4ea5e7995d4a0c037562076c3
Svetlana Morgunova <smorgunova000@gmail.com>
2023-09-26 22:36:26 +0400

    Add new module for wikimedia

---
+++ kafka-producer-wikimedia/src/main/java/sm/pet/kafka/Main.java
@@ -0,0 +1,8 @@
+package sm.pet.kafka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ kafka-producer-wikimedia/src/main/java/sm/pet/kafka/WikimediaChangesProducer.java
@@ -0,0 +1,5 @@
+package sm.pet.kafka;
+
+public class WikimediaChangesProducer {
+
+}
